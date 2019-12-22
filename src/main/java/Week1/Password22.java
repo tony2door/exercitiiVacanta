fbc6e9ed3efe5cc9480bb44de771683f559b17df
Tony <antoniu.tudor@trip-express.com>
2019-12-22 19:35:59 +0200

    Temperatures23

--- src/main/java/Week1/Password22.java
+++ src/main/java/Week1/Password22.java
@@ -2,7 +2,7 @@ package Week1;
 
 import java.util.Scanner;
 
-public class Password21 {
+public class Password22 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String actualPassword = "dolar";
