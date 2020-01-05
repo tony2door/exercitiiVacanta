b6a046b4c0b7739ba6e0b0f7ab483c3bd5de954a
Tony <antoniu.tudor@trip-express.com>
2020-01-05 22:12:29 +0200

    Sum of Many done. Ex 26

---
+++ src/main/java/Week2/SumOfMany26.java
@@ -0,0 +1,22 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class SumOfMany26 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }
+            sum = sum + read;
+            // DO SOMETHING HERE
+
+            System.out.println("Sum now: " + sum);
+        }
+
+        System.out.println("Sum in the end: " + sum);
+    }
+}
