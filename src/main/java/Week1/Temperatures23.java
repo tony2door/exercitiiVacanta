cea05a3e7b52e10114b6bf469d7572a932ad2096
Tony <antoniu.tudor@trip-express.com>
2020-01-05 21:53:19 +0200

    finished Graph with working window

--- src/main/java/Week1/Temperatures23.java
+++ src/main/java/Week1/Temperatures23.java
@@ -1,7 +1,25 @@
 package Week1;
 
+import java.util.Scanner;
+//Exercise 23.2: Checking
+//Improve your program so that temperatures below -30 degrees or over +40 degrees are ignored and not added to the graph.
 public class Temperatures23 {
     public static void main(String[] args) {
-
+        Scanner reader = new Scanner(System.in);
+        boolean mergeProgramul = true;
+        while(mergeProgramul) {
+            System.out.println("Add temperature: || type 404 to exit");
+            Double number = Double.parseDouble(reader.nextLine());
+            if (number > -30 && number < 40) {
+                Graph.addNumber(number);
+            }
+            else if (number == 404){
+                mergeProgramul = false;
+                System.exit(1);
+            }
+            else {
+                System.out.println("Add a number between -29 and 39!");
+            }
+        }
     }
 }
