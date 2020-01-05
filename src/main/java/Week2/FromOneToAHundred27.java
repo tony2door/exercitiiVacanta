d480b1efa31929c8d8e80cb12a08f56b3b4f0ccb
Tony <antoniu.tudor@trip-express.com>
2020-01-05 22:15:35 +0200

    0to100 done. ex 27

---
+++ src/main/java/Week2/FromOneToAHundred27.java
@@ -0,0 +1,11 @@
+package Week2;
+
+public class FromOneToAHundred27 {
+    public static void main(String[] args) {
+        int number = 0;
+        while (number < 100){
+            number++;
+            System.out.println(number);
+        }
+    }
+}
