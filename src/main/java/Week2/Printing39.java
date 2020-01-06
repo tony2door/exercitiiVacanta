7e7471d6528e30708968bd627801b08e8a91b060
Tony <antoniu.tudor@trip-express.com>
2020-01-06 23:58:53 +0200

    Printing 39 first and second

---
+++ src/main/java/Week2/Printing39.java
@@ -0,0 +1,18 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class Printing39 {
+    public static void main(String[] args) {
+        printStars(5);
+    }
+
+
+    private static void printStars (int amount){
+        int counter = 0;
+        while (counter < amount){
+            System.out.print("*");
+            counter++;
+        }
+    }
+}
